3fefb0572323ef6ff175dcb1d7c478758e03dd18
mark-hmft <166622903+mark-hmft@users.noreply.github.com>
2024-04-21 19:41:27 -0600

    Update travel_recommendation.js
    added console.log statements and change URL to Url

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -23,10 +23,11 @@ function searchCondition() {
         
         // Check if the search text contains keywords
         if (input.includes("country")) {
+            console.log("country");
             jsonData.countries.forEach(country => {
                 country.cities.forEach(city => {
                 resultDiv.innerHTML += `<h2>${city.name}</h2>`;
-                resultDiv.innerHTML += `<img src="./${city.imageURL}" alt="hjh">`;
+                resultDiv.innerHTML += `<img src="./${city.imageUrl}" alt="hjh">`;
                 resultDiv.innerHTML += `<p>${city.description}</p>`;
                 });
             });
@@ -34,17 +35,20 @@ function searchCondition() {
         
     
         if (input.includes("temple")) {
+            console.log("temple");
             jsonData.temples.forEach(temple => {
                 resultDiv.innerHTML += `<h2>${temple.name}</h2>`;
-                resultDiv.innerHTML += `<img src="./${temple.imageURL}" alt="hjh">`;
+                resultDiv.innerHTML += `<img src="./${temple.imageUrl}" alt="hjh">`;
                 resultDiv.innerHTML += `<p>${temple.description}</p>`;
             });
         }
     
         if (input.includes("beach")) {
+            console.log("beach");
             jsonData.beaches.forEach(beach => {
                 resultDiv.innerHTML += `<h2>${beach.name}</h2>`;
-                resultDiv.innerHTML += `<img src="./${beach.imageURL}" alt="hjh">`;
+                console.log(`<img src="${beach.imageUrl}" alt="hjh">`);
+                resultDiv.innerHTML += `<img src="./${beach.imageUrl}" alt="hjh">`;
                 resultDiv.innerHTML += `<p>${beach.description}</p>`;
             });
         } else {
